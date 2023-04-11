4899f7f090d9ea474313b260596cc81728c1a612
javawithlwl <javawithlwl@gmail.com>
2023-04-11 08:33:16 +0530

    Added extra log information

--- src/main/java/com/careerit/jfs/cj/day1/HelloWorld.java
+++ src/main/java/com/careerit/jfs/cj/day1/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Welcome to java world!");
+    for(int i=1;i<=5;i++){
+      System.out.println("Welcome user :"+i);
+    }
   }
 
 }
